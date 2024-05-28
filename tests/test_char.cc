1c884c5e127ec0add69a594cc9c0bad2f7b96db1
Michael Peters <michael24peters@gmail.com>
2024-05-28 14:14:16 +0200

    test(core): print(), updated test_char and _npc.cc

--- tests/test_char.cc
+++ tests/test_char.cc
@@ -8,20 +8,21 @@ TEST_CASE("character constructors") {
   // Fully paramaterized values
   std::string name = "Tharrack";
   int hit_points = 72;
-  std::map<std::string, float> attr = {{"STR", 13.0}, {"INT", 8.0},
-                                       {"WIS", 6.0},  {"DEX", 12.0},
-                                       {"CON", 19.0}, {"CHA", 3.0}};
+  std::map<std::string, float> attr = {{"STR", 13.5},  {"INT", 8.1},
+                                       {"WIS", 6.0},   {"DEX", 12.0},
+                                       {"CON", 18.99}, {"CHA", 3.0}};
   std::map<std::string, float> skills = {
-      {"athletics", 2.0}, {"horseriding", 1.0}, {"trapping", 2.0}};
-  std::map<std::string, float> combat = {{"sword", 3.0},
+      {"athletics", 2.77}, {"horseriding", 1.125}, {"trapping", 2.231}};
+  std::map<std::string, float> combat = {{"sword", 3.91},
                                          {"glaive-guisarme", 4.0}};
-  std::map<std::string, float> languages = {
-      {"northern", 1.5}, {"goblin", 1.0}, {"central", -0.5}};
+  std::map<std::string, float> s_languages = {{"common", 1.5},
+                                              {"beastfolk", 1.0}};
+  std::map<std::string, float> l_languages = {{"old oeridian", 1.5}};
   std::map<std::string, float> religions = {{"death goddess", 1.0},
                                             {"animism", 1.0}};
-  std::map<std::string, float> histories = {{"almes woodlands", 0.5}};
-  std::map<std::string, float> cultures = {{"almes barbarians", 3.0},
-                                           {"great kingdom", 1.0}};
+  std::map<std::string, float> histories = {{"adri forest", 0.67}};
+  std::map<std::string, float> cultures = {{"adri barbarians", 3.0},
+                                           {"flinty hills", 1.0}};
   std::map<std::string, float> supernatural = {};
   std::map<std::string, float> elder_races = {{"mi-go", 1.0}};
 
@@ -30,13 +31,14 @@ TEST_CASE("character constructors") {
                                            {"WIS", 3.0}, {"DEX", 3.0},
                                            {"CON", 3.0}, {"CHA", 3.0}};
   std::map<std::string, float> empty = {};
-  std::map<std::string, float> def_lang = {{"northern", 1.0}};
+  std::map<std::string, float> def_slang = {{"northern", 1.0}};
 
   // Characters
   Character c_def;
   Character c_name("Balthazar");
-  Character c_param(name, hit_points, attr, skills, combat, languages,
-                    religions, histories, cultures, supernatural, elder_races);
+  Character c_param(name, hit_points, attr, skills, combat, s_languages,
+                    l_languages, religions, histories, cultures, supernatural,
+                    elder_races);
   Character c_copy(c_param);
 
   SECTION("default constructor") {
@@ -45,11 +47,12 @@ TEST_CASE("character constructors") {
     REQUIRE(c_def.attr == def_attr);
     REQUIRE(c_def.skills == empty);
     REQUIRE(c_def.combat == empty);
+    REQUIRE(c_def.l_languages == empty);
     REQUIRE(c_def.religions == empty);
     REQUIRE(c_def.histories == empty);
     REQUIRE(c_def.supernatural == empty);
     REQUIRE(c_def.elder_races == empty);
-    REQUIRE(c_def.languages == def_lang);
+    REQUIRE(c_def.s_languages == def_slang);
   }
   SECTION("name constructor") {
     REQUIRE(c_name.name == "Balthazar");
@@ -57,11 +60,12 @@ TEST_CASE("character constructors") {
     REQUIRE(c_name.attr == def_attr);
     REQUIRE(c_name.skills == empty);
     REQUIRE(c_name.combat == empty);
+    REQUIRE(c_name.l_languages == empty);
     REQUIRE(c_name.religions == empty);
     REQUIRE(c_name.histories == empty);
     REQUIRE(c_name.supernatural == empty);
     REQUIRE(c_name.elder_races == empty);
-    REQUIRE(c_name.languages == def_lang);
+    REQUIRE(c_name.s_languages == def_slang);
   }
   SECTION("full parameterized constructor") {
     REQUIRE(c_param.name == name);
@@ -69,7 +73,8 @@ TEST_CASE("character constructors") {
     REQUIRE(c_param.attr == attr);
     REQUIRE(c_param.skills == skills);
     REQUIRE(c_param.combat == combat);
-    REQUIRE(c_param.languages == languages);
+    REQUIRE(c_param.s_languages == s_languages);
+    REQUIRE(c_param.l_languages == l_languages);
     REQUIRE(c_param.religions == religions);
     REQUIRE(c_param.histories == histories);
     REQUIRE(c_param.supernatural == supernatural);
@@ -81,7 +86,8 @@ TEST_CASE("character constructors") {
     REQUIRE(c_param.attr == c_copy.attr);
     REQUIRE(c_param.skills == c_copy.skills);
     REQUIRE(c_param.combat == c_copy.combat);
-    REQUIRE(c_param.languages == c_copy.languages);
+    REQUIRE(c_param.s_languages == c_copy.s_languages);
+    REQUIRE(c_param.l_languages == c_copy.l_languages);
     REQUIRE(c_param.religions == c_copy.religions);
     REQUIRE(c_param.histories == c_copy.histories);
     REQUIRE(c_param.supernatural == c_copy.supernatural);
